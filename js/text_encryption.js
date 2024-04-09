ebd32784132027f4298f9b8989f16710ab678f16
jacobpclouse <clouse.jp@gmail.com>
2024-04-09 18:34:26 -0400

    added initial functions

--- js/text_encryption.js
+++ js/text_encryption.js
@@ -2,30 +2,57 @@
 document.getElementById("report-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent default form submission
 
-    // Get the input value
-    var text = document.getElementById("Description").value;
+    // text to encrypt
+    var inputText = document.getElementById("Description").value;
+    console.log(inputText); 
 
-    // Call the function from the linked JavaScript file
-    printText(text);
+    var inputKey = document.getElementById('inputKey').value;
+    console.log(inputKey);
+
+    // execute main body function
+    mainBody(inputText,inputKey);
+
+
   });
 
-// --- Define the function to print text ---
-function printText(dataToHash) {
-    // console.log("Text submitted:", text);
-    // You can perform any encryption or further processing here
+async function mainBody(inputText,inputKey){
+
+  //
+}
 
 
-    // Sample data to hash
-    // const dataToHash = "Hello, World!";
+// used to generate IV for initial randomness
+function generateIV(){
+  const array = new Uint8Array(256);
+  window.crypto.getRandomValues(array);
+  return array;
+}
 
-    // Convert data to hash into a WordArray
-    const dataWordArray = CryptoJS.enc.Utf8.parse(dataToHash);
+// hashing function
+async function sha256HashFunc(message){
+  const msgBuffer = new TextEncoder().encode(message);                  // encode as UTF 8 
+  const hashBuffer = await crypto.subtle.digest('SHA-256',msgBuffer);   // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer));             // convert ArrayBuffer to Array
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+  // console.log(`256 hash: ${hashHex}`);
+  return hashHex;
+}
 
-    // Calculate SHA-256 hash
-    const hash = CryptoJS.SHA256(dataWordArray);
+// used to download a TEXT file *** 
+function downloadFile(input,name,extension){
+  var blob = new Blob([input], {type: "text/plain;charset=utf-8"});
+  var url = URL.createObjectURL(blob);
+  var link = document.createElement("a");
+  link.href = url;
+  // link.download = 'output.txt'
+  link.download = `${name}.${extension}`;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
-    // Convert hash to a hexadecimal string
-    const hashHex = hash.toString(CryptoJS.enc.Hex);
-
-    console.log("SHA-256 Hash:", hashHex);
-  }
+// used to update the values in the file to display the new value
+function updateHTML(value,idToUpdate){
+  var displayElement = document.getElementById(idToUpdate); // select the element
+  displayElement.innerHTML = value; // change its inner html
+}
