bf8b5feda812548ce5b0786a45c666ac9f2c7396
Jacob Clouse <clouse.jp@gmail.com>
2024-11-28 20:51:09 -0500

    it works!

--- js/image_encryption.js
+++ js/image_encryption.js
@@ -1,38 +1,35 @@
 // js for image encryption
-function executeImageMain() {
-  handleImage("inputImage", inputKeyImage.value); // main function houses subfuctions
+function executeImageMain(){
+    handleImage("inputImage",inputKeyImage.value);  // main function houses subfuctions
 }
 
 // function that does the heavy lifting with encryption and decryption
-function handleImage(imageID, imageKey) {
-  var inputElementImage = document.getElementById(imageID); // get image value
-  // var inputElementKey = document.getElementById(imageKey);   // get key value
-  var inputElementKey = imageKey;
-  // console.log(`imageKey: ${imageKey}`);
+function handleImage(imageID,imageKey){
+    var inputElementImage = document.getElementById(imageID);   // get image value
+    // var inputElementKey = document.getElementById(imageKey);   // get key value
+    var inputElementKey = imageKey;
+    // console.log(`imageKey: ${imageKey}`);
 
-  var file = inputElementImage.files[0];
-  var reader = new FileReader();
 
-  // read data for the image into base64 and then encrypt
-  reader.onload = async function () {
-    let base64data = reader.result;
-    // console.log(base64data); // this is your image in base 64 format (bytes)
-    let base64header = base64data.split(",")[0]; // get the header "data:image/png;base64" no comma
-    let base64image = base64data.split(",")[1]; // get values after header, no comma
+    var file = inputElementImage.files[0];
+    var reader = new FileReader();
 
-    // IV
-    let iv_hash = await sha256HashFunc(generateIV());
-    console.log(`IV Hash: ${iv_hash}`);
+    // read data for the image into base64 and then encrypt
+    reader.onload = async function(){
+        let base64data = reader.result;
+        // console.log(base64data); // this is your image in base 64 format (bytes)
+        let base64header = base64data.split(",")[0]; // get the header "data:image/png;base64" no comma
+        let base64image = base64data.split(",")[1];// get values after header, no comma
 
-    // Encrypt
-    // let returnedEncrypted = await encryptionFunc(base64data,inputElementKey, iv_hash);
-    let returnedEncrypted = await encryptionFunc(
-      base64image,
-      inputElementKey,
-      iv_hash
-    );
-    console.log(`Final Image Blocks: ${returnedEncrypted}`);
-    // updateHTML(returnedEncrypted,"displayValueEncrypted")
+        // IV
+        let iv_hash = await sha256HashFunc(generateIV());
+        console.log(`IV Hash: ${iv_hash}`);
+
+        // Encrypt
+        // let returnedEncrypted = await encryptionFunc(base64data,inputElementKey, iv_hash);
+        let returnedEncrypted = await encryptionFunc(base64image,inputElementKey, iv_hash);
+        console.log(`Final Image Blocks: ${returnedEncrypted}`);
+        // updateHTML(returnedEncrypted,"displayValueEncrypted")
 
         // downloadFile(returnedEncrypted,"encrypted_image","txt"); // download encrypted
 
@@ -46,19 +43,16 @@ function handleImage(imageID, imageKey) {
         };
 
 
-    // Decrypt
-    let decryptedReturned = await decryptionFunc(
-      returnedEncrypted,
-      inputElementKey,
-      iv_hash
-    );
-    console.log(`Decrypted Image: ${decryptedReturned}`);
-    // updateHTML(decryptedReturned,"displayValueDecrypted");
+        // Decrypt
+        let decryptedReturned = await decryptionFunc(returnedEncrypted, inputElementKey, iv_hash);
+        console.log(`Decrypted Image: ${decryptedReturned}`);
+        // updateHTML(decryptedReturned,"displayValueDecrypted");
 
-    // create image from Base64 decrypted string
-    createImageFromBase64(decryptedReturned, base64header);
-  };
-  reader.readAsDataURL(file);
+        // create image from Base64 decrypted string
+        createImageFromBase64(decryptedReturned,base64header)
+        
+    }
+    reader.readAsDataURL(file);
 }
 
 // // create image from base 64 string function -- jpeg
